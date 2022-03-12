2a9f3ee92c8fd83b9c350574b3ee85f073959591
Nate Marti <nate_marti@sil.org>
2022-03-12 06:52:12 +0100

    added manpage

---
+++ bin/manpage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Preview and convert MD file to manpage.
+# Ref:
+#   https://www.howtogeek.com/682871/how-to-create-a-man-page-on-linux/
+#   https://github.com/jgm/pandoc/issues/5404
+
+this_script_dir=$(realpath $(dirname "${0}"))
+root_dir=$(dirname "$this_script_dir")
+app_name=$(basename "$root_dir")
+draft="${root_dir}/data/${app_name}.1.md"
+outfile_name="${draft%.*}"
+outfile_name="${outfile_name##*/}"
+outfile="${root_dir}/data/${outfile_name}"
+debfile="${root_dir}/debian/${app_name}.manpages"
+if [[ -z "$1" || "$1" == 'preview' || "$1" == 'p' ]]; then
+    pandoc -s -f markdown-smart -t man "$draft" | man -l -
+elif [[ "$1" == 'convert' || "$1" == 'c' ]]; then
+    mkdir -p "${root_dir}/data"
+    pandoc -s -f markdown-smart -t man -o "$outfile" "$draft"
+    echo "data/${outfile_name}" > "$debfile"
+fi
---
+++ data/flash-sync.1.md
@@ -0,0 +1,41 @@
+% FLASH-SYNC(1)
+
+# NAME
+**Flash Sync**
+
+Synchronize files to a flash drive intended for use in a simple MP3 player.
+
+# SYNOPSIS
+**flash-sync** [OPTION...] [SOURCE] DEST
+
+# OPTIONS
+**-c**
+: Clean (remove all files in) DEST
+
+**-d**
+: Print DEBUG info to stdout.
+
+**-h**
+: Show help and exit.
+
+**-i**
+: Install script and desktop launcher (if running from source).
+
+**-l**
+: List files in DEST in disk order.
+
+**-n**
+: Fix filenames in DEST to exclude special characters
+
+**-s**
+: Update FAT to sort files by name in DEST
+
+**-u**
+: Uninstall script and desktop launcher (if installed using '-i')
+
+# BUGS
+| Bug reports and issue tracker:
+| <https://github.com/wasta-linux/flash-sync/issues>
+
+# AUTHOR
+**flash-sync** was written by Nate Marti <https://github.com/n8marti/>.
